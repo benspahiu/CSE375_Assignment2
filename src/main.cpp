b9534a11d73cfdb2c4b5f3eefb4c2fd3115a7032
Ben Spahiu <bes226@lehigh.edu>
2025-10-27 20:17:36 -0400

    Shifted structure to template code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+#include "hashes.h"
+#include "cuckoo_seq.h"
 int main(){
+  std::cout << myhash::StdHash1<std::string>{}("abcd") << std::endl;
+  std::cout << myhash::StdHash2<std::string>{}("abcd") << std::endl;
+  cuckoo::cuckoo_seq<std::string> set;
+  std::cout << set.insert("A") << std::endl;
   return 0;
 }
